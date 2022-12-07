90bb835465648be8a04c6fce826712d301ccb7cd
Arth Vidyarthi <arthvid@berkeley.edu>
2022-12-06 20:25:01 -0600

    CNN Block added

--- model.py
+++ model.py
@@ -21,3 +21,17 @@ architecture_config = [
     (3, 1024, 1, 1),
     (3, 1024, 1, 1),
 ]
+
+class ConvLayer(nn.Module):
+
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        return self.leakyrelu(self.batchnorm(self.conv(x)))
+
+
+clas
